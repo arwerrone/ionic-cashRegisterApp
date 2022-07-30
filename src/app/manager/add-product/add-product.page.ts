c3df862054b765dad635dfdb49ded16eea8f6ad8
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2022-07-29 23:41:08 -0300

    Add New Product finished

--- src/app/manager/add-product/add-product.page.ts
+++ src/app/manager/add-product/add-product.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService, Item } from 'src/app/item.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductPage implements OnInit {
 
-  constructor() { }
+  prodctName: string;
+  productPrice: number;
+  productQuantity: number;
+
+  currentItem: Item = {
+    id: -1,
+    name: 'Type',
+    quantity: -1,
+    price: -1,
+  };
+
+  constructor(private itemService: ItemService, private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  async saveClicked(){
+    if(this.prodctName && this.productPrice && this.productQuantity){
+      
+      this.currentItem.id = Date.now();
+      this.currentItem.name = this.prodctName;
+      this.currentItem.quantity = this.productQuantity;
+      this.currentItem.price = this.productPrice;
+      this.itemService.addItem(this.currentItem);
+
+      this.router.navigate(['/manager']);
+
+      const alert = await this.alertController.create({
+        header: 'Done!',
+        message: 'New Product Added Successfully',
+        //buttons: ['OK']
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            this.router.navigate(['/home']);
+          }
+        }]
+      });
+  
+      await alert.present();
+
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'You have to provide Product Name, Price and Quantity',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
   }
 
 }
